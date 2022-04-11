26ec5c7ca7d204c1862945bb4c60405ca7a86a8a
jkim makes <jkim.makes@gmail.com>
2022-04-10 19:55:16 -0700

    Make a black vernier

--- vernier.py
+++ vernier.py
@@ -11,7 +11,7 @@ from svgwrite import mm
 collar_diameter = 110
 
 p_tick_size = collar_diameter * math.pi / 100
-vernier_tick_size = p_tick_size / 9 * 10
+vernier_tick_size = p_tick_size * 9 / 10
 
 # Sizes of the ticks on the p100 dial
 p_major_tick_width = 1.25
@@ -25,8 +25,23 @@ v_fives_tick_length = 6
 v_ones_tick_length = 3
 
 # Colors
-tick_color='black'
-bg_color='white'
+tick_color='white'
+bg_color='black'
+
+
+def draw_p100_tick(dwg, x, y, outer_width, inner_width):
+    """
+    Draws a nested tick
+    """
+    dwg.add(dwg.rect(
+        (x*mm, y*mm),  # starting position
+        (outer_width*mm, p_major_tick_width*mm),  # size
+        fill=tick_color))
+    # Draw interor fill on major ticks w/ thin white boxes for lining up zero to a minor tick on p100
+    dwg.add(dwg.rect(
+        (x*mm, (y + tick_offset) * mm),  # starting position
+        (inner_width*mm, p_minor_tick_width*mm),  # size
+        fill=bg_color))
 
 
 def draw_vernier(dwg, x, y):
@@ -38,44 +53,39 @@ def draw_vernier(dwg, x, y):
     :param y: int, y offset
     """
     # Draw major Ticks
-    dwg.add(dwg.rect(
-        (x*mm, y*mm),  # starting position
-        (v_tens_tick_length*mm, p_major_tick_width*mm),  # size
-        fill=tick_color))
-    dwg.add(dwg.rect(
-        (x*mm, (vernier_tick_size * 10 + y) * mm),
-        (v_tens_tick_length*mm, p_major_tick_width*mm),
-        fill=tick_color))
+    draw_p100_tick(dwg, x, y, v_tens_tick_length, v_fives_tick_length)
+
+    # Make second major tick shorter to emphasize that it's the end, not the
+    # start of a vernier
+    draw_p100_tick(dwg, x, vernier_tick_size * 10 + y, v_fives_tick_length,
+                   v_ones_tick_length)
 
     # Draw minor ticks
-    for i in range(2, 10):
-        dwg.add(dwg.rect(
-            (x*mm, (vernier_tick_size * i + y + tick_offset)*mm),
-            (v_ones_tick_length*mm, p_minor_tick_width*mm),
-            fill=tick_color))
+    for i in range(1, 10):
+        draw_p100_tick(dwg, x, vernier_tick_size * i + y, v_ones_tick_length, v_ones_tick_length)
 
     # Draw fives ticks
-    dwg.add(dwg.rect(
-        (x*mm,(vernier_tick_size * 5 + y + tick_offset)*mm),
-        (v_fives_tick_length*mm, p_minor_tick_width*mm),
-        fill=tick_color))
-
-    # Draw interor fill on major ticks w/ thin white boxes for lining up zero to a minor tick on p100
-    dwg.add(dwg.rect(
-        (x*mm, (y + tick_offset) * mm),  # starting position
-        (v_fives_tick_length*mm, p_minor_tick_width*mm),  # size
-        fill=bg_color))
-    dwg.add(dwg.rect(
-        (x*mm, (vernier_tick_size * 10 + y + tick_offset) * mm),
-        (v_fives_tick_length*mm, p_minor_tick_width*mm),
-        fill=bg_color))
+    draw_p100_tick(dwg, x, vernier_tick_size * 5 + y, v_fives_tick_length,
+                   v_ones_tick_length)
 
 
 def main():
     dwg = svgwrite.Drawing('vernier.svg', height='200mm', width='100mm')
 
+    # Draw a black background box
+    dwg.add(dwg.rect(
+        (0*mm, 0*mm),  # starting position
+        (40*mm, 300*mm),  # size
+        fill=bg_color))
+
+    # Draw a series of verniers
     for i in range(0, 7):
         draw_vernier(dwg, 10, 10+(p_tick_size*10) * i)
+
+    # Add a '*' to the vernier that I want to be used as the zero marker
+    dwg.add(dwg.text('*', insert=(16*mm, (9 + p_tick_size*10 * 3 ) * mm),
+                     stroke='none', fill=tick_color,
+                     font_size='10mm', font_family="Helvetica", rotate=[90]))
 
     dwg.save()
 
